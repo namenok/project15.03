411ff16981b8c1164cc47be7e9e3e0de9376a7dd
orynka <arinaznamenok@gmail.com>
2025-06-30 19:52:25 +0300

    CI

--- websiteProject/settings.py
+++ websiteProject/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }

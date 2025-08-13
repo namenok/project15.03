cab9066ea828ffb3ac052e556f7a79902e88662a
namenok <comfykh810@gmail.com>
2025-08-13 17:13:33 +0300

    trustedOriginCsrfAdded-settings

--- websiteProject/settings.py
+++ websiteProject/settings.py
@@ -150,3 +150,11 @@ X_FRAME_OPTIONS = "DENY"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8080",
+    "http://localhost:8080",
+    "http://10.0.2.15:8000",
+]
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')

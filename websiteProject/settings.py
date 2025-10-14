93306ae3220b65e7babdd7c58056457734a10b86
oryna-server <orynazn@gmail.com>
2025-10-14 13:39:08 +0000

    fr server

--- websiteProject/settings.py
+++ websiteProject/settings.py
@@ -168,7 +168,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
 SPOTIFY_CLIENT_ID = "456e8268198e49d8a6418853652cd98b"
 SPOTIFY_CLIENT_SECRET = "767e5d97fadc40f8aa80670dfb2e98e0"
 
-SPOTIFY_REDIRECT_URI = "https://22995ce867fb.ngrok-free.app/en-gb/spotify/callback/"
+SPOTIFY_REDIRECT_URI = "https://8c200336a887.ngrok-free.app/en-gb/spotify/callback/"
 
 SPOTIFY_SCOPES = (
     "streaming "
